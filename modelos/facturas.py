b1836aa8d2c265d2b2d1b1189fd5daf8f42e48b6
Valentina Nausa <Valentinanausa@gmail.com>
2026-06-25 21:36:11 -0500

    implementación de lógica de creación de facturas y validación de clientes y campos computados

--- modelos/facturas.py
+++ modelos/facturas.py
@@ -1,10 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
+from typing import List
+from datetime import datetime
 from .clientes import Cliente
+from .transacciones import Transaccion
 
 class FacturaBase(BaseModel):
-    cliente: Cliente
-    monto: float
-    descripcion: str
+    
+    cliente: Cliente | None = None
+    fecha: datetime = datetime.now()
+    transacciones: List[Transaccion] = []
+
+    @computed_field
+    @property
+    def valor_total(self) -> float:
+        return 0.0  
 
 class Factura(FacturaBase):
     id: int | None = None
